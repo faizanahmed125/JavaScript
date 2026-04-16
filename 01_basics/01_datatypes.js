0a9d1f48eaafedb3afa24fcd1e4b1c608269f322
faizan_dev <shafahd125@gmail.com>
2026-04-16 14:00:02 +0000

    data types

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all js code as newer js
+
+// type of null is an object
+//type of undefined is undefined
+// string, number, bigInt, boolean, null (standalone value), 
+// undefined (value not assigned yet), symbol (unique) 
